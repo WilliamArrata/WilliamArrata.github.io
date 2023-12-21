d08f5ad0842fe1056e8916bda0c975ef797567de
William Arrata <125134722+WilliamArrata@users.noreply.github.com>
2023-12-21 21:45:39 +0100

    Update risk_diversification_ESSEC.R

--- risk_diversification_ESSEC.R
+++ risk_diversification_ESSEC.R
@@ -24,34 +24,38 @@ plot(pair[[1]][c(1,length(mean)),], las=1,xlab="", ylab="",xlim=xlim, ylim=ylim,
 mapply(lines, pair, col=colv,lty=lty)
 mapply(title, c(expression(E(r[P]) ("%"),sigma(r[P]) ("%"),"A","B")),
        adj=c(0,1,0.45,0.9),line=c(-1,-22,-18,-2))
-legend("bottom", horiz=T,inset = c(0,-0.2),text.col=colv,pch=rep(NA,4),lty=c(rep(1,3),3),col=colv, bty="n",
-       legend= expression(paste(rho[A][B]," = ",-1),paste(rho[A][B]," = ",1),paste(rho[A][B]," = ",0),paste(rho[A][B]," = ",0.25)))
+legend("bottom", horiz=T,inset = c(0,-0.2),text.col=colv,pch=rep(NA,4),lty=c(rep(1,3),3),col=colv, bty="n", legend =
+         expression(paste(rho[A][B]," = -1"),paste(rho[A][B]," = 1"),paste(rho[A][B]," = 0"),paste(rho[A][B]," = 0.25")))
 
 #######################################   COMBINING ASSET BY PAIRS   ##################################
 
 w_a<-seq(0,1,length.out=300)
-w_a<-cbind(w_a,rev(w_a))                                        #a range of weights for each asset in the portfolio
+w_a<-cbind(w_a,rev(w_a))                                           #a range of weights for each asset in the portfolio
 
-mu_c<-list(c(0.03,0.05),c(0.05,0.065),c(0.065,0.075),              #expected returns
-           c(0.075,0.11),c(0.11,0.15),c(0.03,0.15))
-sig<-list(c(0.05,0.08,0.03),c(0.08,0.11,-0.03),c(0.11,0.12,0.06),  #covariances
-          c(0.12,0.16,0.07),c(0.16,0.21,0.12),c(0.05,0.21,-0.08))
+mu_c <- list(c(0.03,0.05), c(0.05,0.065), c(0.065,0.075),          #expected returns on pairs of assets
+             c(0.075,0.11), c(0.11,0.15), c(0.03,0.15))
+sig <- list(c(0.05,0.08,0.03), c(0.08,0.11,-0.03), c(0.11,0.12,0.06),#covariances between pairs of assets
+            c(0.12,0.16,0.07), c(0.16,0.21,0.12), c(0.05,0.21,-0.08))
 
-mu_tot<-sd_tot<-pair_tot<-list()                          #portfolios' expected returns and standard deviations
+mu_tot <- sd_tot <- pair_tot <- list()      #Expected returns & standard deviations on portfolios of two assets
 for (i in seq_along(mu_c)){
-  mu_tot[[i]]<-100*w_a%*%mu_c[[i]]
-  sd_tot[[i]]<-100*sqrt(diag(w_a%*%matrix(sig[[i]][c(1,3,3,2)],nrow=2)%*%t(w_a)))
-  pair_tot[[i]]<-cbind(sd_tot[[i]],mu_tot[[i]])}
+  mu_tot[[i]] <- 100*w_a%*%mu_c[[i]]
+  sd_tot[[i]] <- 100*sqrt(diag(w_a%*%matrix(sig[[i]][c(1,3,3,2)], nrow=2)%*%t(w_a)))
+  pair_tot[[i]] <- cbind(sd_tot[[i]],mu_tot[[i]])}
+
+corner <- data.frame(unique(cbind(unlist(c(lapply(sd_tot, last),lapply(sd_tot, first))),
+                                  unlist(c(lapply(mu_tot, last),lapply(mu_tot, first))))))
+frontier <- data.frame(do.call(rbind, pair_tot))
+colnames(frontier) <- colnames(corner) <- c("stdev", "mean")
+frontier <- frontier[order(frontier$mean),]
 
 #graph
-xaxis<-range(sd_tot)*c(0.9,1.1)
-yaxis<-range(mu_tot)*c(0.9,1.1)
-adj<-c(0.92,0.77,0.64,0.6,0.5,0.37)
-line<-c(-1.4,-6.3,-9.8,-11.2,-12.25,-14.7)
-col<-c(rainbow(length(mu_c)-1),"black")
 asset<-LETTERS[seq_along(mu_c)]
 
-par(mar=c(4,5,4,3))
-plot(NA, xlab="standard deviation (%)", ylab="expected return (%)",xlim=xaxis, ylim=yaxis, las=1)
-mapply(lines, pair_tot, col=col,pch=20)
-mapply(title, asset, adj=adj,line=line)
+ggplot() +
+  xlim(range(sd_tot)*c(0.9,1.1)) + ylim(range(mu_tot)*c(0.9,1.1)) +
+  geom_segment(data = frontier, aes(x = stdev, xend = dplyr::lead(stdev),
+                                    y = mean, yend = dplyr::lead(mean)), size = 1, color="indianred") +
+  geom_point(data = corner, aes(stdev, mean), color="black", size=3) +
+  annotate(geom="text", x = corner$stdev,  y = corner$mean, label=asset, fontface = 2, hjust=-0.6, vjust=1.2) +
+  labs(x="standard deviation (%)", y="expected return (%)") +  theme(plot.margin = margin(1.2,.5,1.2,.5, "cm"))
