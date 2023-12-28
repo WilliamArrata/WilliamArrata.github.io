eca60afbf7b0aa4035cbd1a15db5e2dc274d35a5
William Arrata <125134722+WilliamArrata@users.noreply.github.com>
2023-12-28 22:21:12 +0100

    Update feasible_set_ESSEC.R

--- feasible_set_ESSEC.R
+++ feasible_set_ESSEC.R
@@ -1,30 +1,28 @@
 
-############   WILLIAM ARRATA - FEASIBLE SET - ESSEC PORTFOLIO MANAGEMENT COURSE WINTER 2023   #############
+#####################   WILLIAM ARRATA - ESSEC PORTFOLIO MANAGEMENT COURSE WINTER 2023   #####################
 
 require("pacman")
-pacman::p_load("readxl","ggplot2","tibble")
+pacman::p_load("readxl","ggplot2","tibble","dplyr","data.table")
 
 #############################################  FEASIBLE SET #######################################################
 
 #load data and calculate annualized historical returns and covariances
-donnees<-as.data.frame(read_excel("stock_prices.xlsx",1))          #load stock prices
-ret<-apply(donnees[,-1],2,diff)/donnees[-1,-1]                     #daily historical returns
-moy<-252*matrix(colMeans(ret))                                     #annualized expected returns
-cov_mat<-252*cov(ret)                                              #annualized covariances
+ret <- as.matrix(read_excel("stock_prices.xlsx") %>%  select_if(is.numeric) %>% 
+                   mutate_all(~ ( (.) - shift(.))/(.)) %>% na.omit())
+moy <- 252*matrix(colMeans(ret))                                     #annualized expected returns
+cov_mat <- 252*as.matrix(cov(ret))                                   #annualized covariances
 
-w<-seq(from=0,to=1,by=0.1)                                         #Create sets of positive weights
-w<-expand.grid(rep(list(w),length(moy)))                           #Short selling not allowed
-w<-as.matrix(w[rowSums(w)==1,])
-returns<-rowSums(w%*%moy)                                          #Portfolios' return
-risk<-diag(sqrt(w%*%cov_mat%*%t(w)))                               #Portfolios' risk
-sharpe<-returns/risk                                               #Portfolios' Sharpe Ratio
-portfolio<-tibble(Return=returns,Risk=risk,SharpeRatio=sharpe)     #Storing values in table
+w <- list(seq(from = 0, to = 1, by = 0.1))                           # Create sets of weights for the 6 assets
+w <- expand.grid(rep(w, length(moy)))
+w <- as.matrix(w[rowSums(w)==1,])
+
+returns <- rowSums(w%*%moy)                                          # Portfolios' return
+stdev <- diag(sqrt(w%*%cov_mat%*%t(w)))                              # Portfolios' risk
+portfolio <- tibble(Return = returns, Risk = stdev)                  # Storing values in table
 
 #Graph in the mean standard deviation space of all created portfolios
-p <- portfolio %>%
-  ggplot(aes(x = Risk, y = Return, color = SharpeRatio)) +
+ggplot(portfolio, aes(x = Risk, y = Return)) +
   geom_point() +
-  theme_classic() +
   scale_y_continuous(labels = scales::percent) +
   scale_x_continuous(labels = scales::percent) +
   labs(x = 'Annualized Standard Deviations', y = 'Annualized Returns')
