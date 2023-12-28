254156a93da45acc5639cd075b3e9146fa3458b0
William Arrata <125134722+WilliamArrata@users.noreply.github.com>
2023-12-28 22:22:26 +0100

    Update risk_diversification_ESSEC.R

--- risk_diversification_ESSEC.R
+++ risk_diversification_ESSEC.R
@@ -1,7 +1,12 @@
 
+#####################   WILLIAM ARRATA - ESSEC PORTFOLIO MANAGEMENT COURSE WINTER 2023   ################
+
+require("pacman")
+pacman::p_load("data.table","dplyr","ggplot2","binhf")
+
 #######################   VARYING THE CORRELATION COEFFICIENT BETWEEN TWO ASSETS    ###############################
 
-mu <- c(3,7)*1e-2                                                     #Expected returns on assets A and B
+m <- c(3,7)*1e-2                                                      #Expected returns on assets A and B
 V <- c(1.44, 6.25)*1e-2                                               #Variance on assets A and B
 rho <- c(-1, 1, 0, 0.25)                                              #different values for the correlation
 sd <- apply(cbind(V[1],replicate(2,sqrt(prod(V))*rho),V[2]),1,list)   #all covariances matrix
@@ -10,7 +15,7 @@ w <- seq(0, 1, length.out = 300)
 w <- cbind(w, rev(w))                                                 #a range of weights for both assets
 
 
-mean <- 100*w%*%mu                                                                 #portfolio expected returns
+mean <- 100*w%*%m                                                                 #portfolio expected returns
 pair<-list()                               
 for (i in seq_along(rho)){
   pair[[i]]<-cbind(100*sqrt(diag(w%*%matrix(sd[[i]][[1]],nrow=2)%*%t(w))) ,mean)}  #portfolio variance
@@ -27,42 +32,57 @@ mapply(lines, pair, col = colv, lty = lty)
 mapply(title, c(expression(E(r[P]) ("%"),sigma(r[P]) ("%"),"A","B")),
        adj=c(0,1,0.45,0.9),line=c(-1,-22,-18,-2))
 legend("bottom", horiz=T, inset = c(0,-0.3), text.col = colv, pch = rep(NA,4), lty = c(rep(1,3),3), col = colv, 
-       bty="n", legend = expression(paste(rho[A][B]," = -1"), paste(rho[A][B]," = 1"), paste(rho[A][B]," = 0"), 
-                                    paste(rho[A][B]," = 0.25")))
+       bty="n", legend = parse(text = sprintf("rho[A][B] == %s",rho)))
 
 
 #######################################   COMBINING ASSET BY PAIRS   ##################################
 
-mu_c <- list(c(0.03,0.05), c(0.05,0.065), c(0.065,0.075),             #expected returns on pairs of assets
-             c(0.075,0.11), c(0.11,0.15), c(0.03,0.15))
+mu <- list(c(0.03,0.05), c(0.05,0.065), c(0.065,0.075),             #expected returns on pairs of assets
+           c(0.075,0.11), c(0.11,0.15), c(0.03,0.15))
 sig <- list(c(0.05,0.08,0.03), c(0.08,0.11,-0.03), c(0.11,0.12,0.06), #covariances between pairs of assets
             c(0.12,0.16,0.07), c(0.16,0.21,0.12), c(0.05,0.21,-0.08))
 
+#assets coordinates
+assets <- 100*data.frame(stdev = unique(sqrt(unlist(lapply(sig, function(x) head(x,2))))), mean = unique(unlist(mu)))
+
 w_a <- seq(0, 1, length.out = 300)
-w_a <- cbind(w_a, rev(w_a))                                           #a range of weights for each asset in the portfolio
+w_a <- cbind(w_a, rev(w_a))       #a range of weights for each asset in the portfolio, varying by steps of 1/300
 
-mu_tot <- sd_tot <- pair_tot <- list()      #Expected returns & standard deviations on portfolios of two assets
-for (i in seq_along(mu_c)){
-  mu_tot[[i]] <- 100*w_a%*%mu_c[[i]]
+mu_tot <- sd_tot <- pair_tot <- list() #Expected returns & standard deviations on combinations of 2 consecutive assets
+for (i in seq_along(mu)){
+  mu_tot[[i]] <- 100*w_a%*%mu[[i]]
   sd_tot[[i]] <- 100*sqrt(diag(w_a%*%matrix(sig[[i]][c(1,3,3,2)], nrow=2)%*%t(w_a)))
-  pair_tot[[i]] <- cbind(sd_tot[[i]],mu_tot[[i]])
-  colnames(pair_tot[[i]]) <- c("stdev", "mean")}
+  pair_tot[[i]] <- data.frame( stdev = sd_tot[[i]], mean = mu_tot[[i]])}
 
-corner <- data.frame(unique(cbind(unlist(c(lapply(sd_tot, last),lapply(sd_tot, first))),
-                                  unlist(c(lapply(mu_tot, last),lapply(mu_tot, first))))))
-frontier <- data.frame(do.call(rbind, pair_tot[-length(pair_tot)]))
-colnames(corner) <- colnames(frontier)
-frontier <- frontier[order(frontier$mean),]
+assets$correl <- unlist(lapply(sig, last))/sqrt(mapply(prod, lapply(sig, function(x) head(x,2))))
 
-#graph
-asset<-LETTERS[seq_along(mu_c)]
+#the resulting curves
+curve <- do.call(rbind, pair_tot[-length(pair_tot)])
+curve <- curve[order(curve$mean),]
+curve_2 <- pair_tot[length(pair_tot)][[1]]
 
-ggplot() +
-  xlim(range(sd_tot)*c(0.9,1.1)) + ylim(range(mu_tot)*c(0.9,1.1)) +
-  geom_segment(data = frontier, aes(x = stdev, xend = dplyr::lead(stdev),
-                                    y = mean, yend = dplyr::lead(mean)), size = 1, color="indianred") +
-  geom_segment(data = data.frame(pair_tot[length(pair_tot)]), aes(x = stdev, xend = dplyr::lead(stdev),
-                                                                  y = mean, yend = dplyr::lead(mean)), size = 1) +
-  geom_point(data = corner, aes(stdev, mean), size=3) +
-  annotate(geom="text", x = corner$stdev,  y = corner$mean, label=asset, fontface = 2, hjust=-0.6, vjust=1.2) +
-  labs(x="standard deviation (%)", y="expected return (%)") +  theme(plot.margin = margin(1.2,.5,1.2,.5, "cm"))
+#Graph of curves formed by consecutive pairs of assets
+ggplot(curve, aes(stdev, mean)) +
+  xlim(c(0.9,1.1)*range(curve$stdev)) + ylim(c(0.9,1.1)*range(curve$mean)) +
+  geom_point(color="blue", size = 0.5) + geom_point(data = assets, aes(stdev, mean), size=3) +
+  geom_text(data = assets, aes(x=stdev, y=mean, label = LETTERS[seq_along(mu)]), hjust = -0.6, vjust = 1.2, fontface = 2) +
+  labs(x = "standard deviation (%)", y = "expected return (%)") + theme(plot.margin = margin(1.2,.5,1.2,.5, "cm"))
+
+#Adding in the graph the curve between most distant assets
+ggplot(data =  assets, aes(x = stdev, y = mean)) +
+  xlim(c(0.9,1.1)*range(curve_2$stdev)) + ylim(c(0.9,1.1)*range(curve_2$mean)) +
+  geom_point(aes(fill = as.factor(correl))) +
+  geom_segment(data = curve, aes(x = stdev, xend = dplyr::lead(stdev), y = mean, yend = dplyr::lead(mean)), 
+               size = 1, color="indianred") +
+  geom_segment(data = curve_2, aes(x = stdev, xend = dplyr::lead(stdev), y = mean, yend = dplyr::lead(mean)),size = 1) +
+  geom_point(data = assets, aes(stdev, mean), size = 3) +
+  annotate(geom = "text", x = assets$stdev,  y = assets$mean, label = LETTERS[seq_along(mu)], 
+           fontface = 2, hjust = -0.6, vjust = 1.2) +
+  scale_fill_manual(
+    values = rep("black", 6), labels = parse(text = sprintf("rho == %s", round(assets$correl,2), drop = F))) +
+  # scale_fill_manual(
+  #   values = rep("black",6),
+  #   labels = paste0(expression(rho),"_",LETTERS[seq_along(mu)],",",shift(LETTERS[seq_along(mu)], places = -1)," : ",
+  #                   round(correl,2)), drop = F) +
+  labs(x = "standard deviation (%)", y = "expected return (%)", fill=NULL) +
+  theme(legend.position = "bottom", plot.margin = margin(1.2,.5,1.2,.5, "cm"))
