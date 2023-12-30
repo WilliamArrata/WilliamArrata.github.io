7cc3a9e7302b983fd0298b9ddfb31d3514f7a60e
William Arrata <125134722+WilliamArrata@users.noreply.github.com>
2023-12-30 11:55:58 +0100

    Update risk_diversification_ESSEC.R

--- risk_diversification_ESSEC.R
+++ risk_diversification_ESSEC.R
@@ -2,36 +2,36 @@
 #####################   WILLIAM ARRATA - ESSEC PORTFOLIO MANAGEMENT COURSE WINTER 2023   ################
 
 require("pacman")
-pacman::p_load("data.table","dplyr","ggplot2","binhf")
+pacman::p_load("data.table","dplyr","ggplot2")
 
 #######################   VARYING THE CORRELATION COEFFICIENT BETWEEN TWO ASSETS    ###############################
 
 m <- c(3,7)*1e-2                                                      #Expected returns on assets A and B
 V <- c(1.44, 6.25)*1e-2                                               #Variance on assets A and B
 rho <- c(-1, 1, 0, 0.25)                                              #different values for the correlation
-sd <- apply(cbind(V[1],replicate(2,sqrt(prod(V))*rho),V[2]),1,list)   #all covariances matrix
+cov <- apply(cbind(V[1],replicate(2,sqrt(prod(V))*rho),V[2]),1,list)  #all covariances matrix
 
 w <- seq(0, 1, length.out = 300)
 w <- cbind(w, rev(w))                                                 #a range of weights for both assets
 
-
-mean <- 100*w%*%m                                                                 #portfolio expected returns
+mean <- 100*w%*%m                                                     #portfolio expected returns
 pair<-list()                               
 for (i in seq_along(rho)){
-  pair[[i]]<-cbind(100*sqrt(diag(w%*%matrix(sd[[i]][[1]],nrow=2)%*%t(w))) ,mean)}  #portfolio variance
+  sd[[i]] <- 100*sqrt(diag(w%*%matrix(cov[[i]][[1]],nrow=2)%*%t(w)))  #portfolio variance
+  pair[[i]]<-data.frame(stdev = sd[[i]], mean = mean)}                
 
 #Graph of the 300 portfolios in the mean standard deviation space
-xlim <- range(do.call(rbind,pair)[,1])*1.1
-ylim <- range(mean)*c(0.8,1.1)
+xlim <- 1.1*range(sd)
+ylim <- c(0.8,1.1)*range(mean)
 colv <- c(rainbow(length(rho)-1),"black")
 lty <- rep(1:2,c(length(rho)-1,1))
 
-par(mar=c(6, 4, 4, 3),xpd=T)
+par(mar=c(7, 4, 4, 3),xpd=T)
 plot(pair[[1]][c(1,length(mean)),], las=1, xlab="", ylab="", xlim = xlim, ylim = ylim, pch = 20)
 mapply(lines, pair, col = colv, lty = lty)
-mapply(title, c(expression(E(r[P]) ("%"),sigma(r[P]) ("%"),"A","B")),
-       adj=c(0,1,0.45,0.9),line=c(-1,-22,-18,-2))
-legend("bottom", horiz=T, inset = c(0,-0.3), text.col = colv, pch = rep(NA,4), lty = c(rep(1,3),3), col = colv, 
+mapply(mtext, c(expression(sigma(r[P]) ("%"), E(r[P]) ("%"))), side=c(1,2), line = rep(2.5,2))
+text(x = 1.1*pair[[1]]$stdev[c(1,length(mean))], y = pair[[1]]$mean[c(1,length(mean))], label = c("B","A"), font =2)
+legend("bottom", horiz=T, inset = c(0,-0.4), text.col = colv, pch = rep(NA,4), lty = c(rep(1,3),3), col = colv, 
        bty="n", legend = parse(text = sprintf("rho[A][B] == %s",rho)))
 
 
@@ -43,7 +43,9 @@ sig <- list(c(0.05,0.08,0.03), c(0.08,0.11,-0.03), c(0.11,0.12,0.06), #covarianc
             c(0.12,0.16,0.07), c(0.16,0.21,0.12), c(0.05,0.21,-0.08))
 
 #assets coordinates
-assets <- 100*data.frame(stdev = unique(sqrt(unlist(lapply(sig, function(x) head(x,2))))), mean = unique(unlist(mu)))
+assets <- 100*data.frame(stdev = unique(sqrt(unlist(lapply(sig, function(x) head(x,2))))), 
+                         mean = unique(unlist(mu)),
+                         correl = unlist(lapply(sig, last))/sqrt(mapply(prod, lapply(sig, function(x) head(x,2)))))
 
 w_a <- seq(0, 1, length.out = 300)
 w_a <- cbind(w_a, rev(w_a))       #a range of weights for each asset in the portfolio, varying by steps of 1/300
@@ -53,8 +55,6 @@ for (i in seq_along(mu)){
   mu_tot[[i]] <- 100*w_a%*%mu[[i]]
   sd_tot[[i]] <- 100*sqrt(diag(w_a%*%matrix(sig[[i]][c(1,3,3,2)], nrow=2)%*%t(w_a)))
   pair_tot[[i]] <- data.frame( stdev = sd_tot[[i]], mean = mu_tot[[i]])}
-
-assets$correl <- unlist(lapply(sig, last))/sqrt(mapply(prod, lapply(sig, function(x) head(x,2))))
 
 #the resulting curves
 curve <- do.call(rbind, pair_tot[-length(pair_tot)])
@@ -79,7 +79,7 @@ ggplot(data =  assets, aes(x = stdev, y = mean)) +
   annotate(geom = "text", x = assets$stdev,  y = assets$mean, label = LETTERS[seq_along(mu)], 
            fontface = 2, hjust = -0.6, vjust = 1.2) +
   scale_fill_manual(
-    values = rep("black", 6), labels = parse(text = sprintf("rho == %s", round(assets$correl,2), drop = F))) +
+    values = rep("black", 6), labels = parse(text = sprintf("rho == %s", round(assets$correl/100,2), drop = F))) +
   # scale_fill_manual(
   #   values = rep("black",6),
   #   labels = paste0(expression(rho),"_",LETTERS[seq_along(mu)],",",shift(LETTERS[seq_along(mu)], places = -1)," : ",
