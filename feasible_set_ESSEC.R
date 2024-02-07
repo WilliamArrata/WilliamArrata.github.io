2a1f70a799c29b029db5d85763bd0ecda8b8aa49
William Arrata <125134722+WilliamArrata@users.noreply.github.com>
2024-02-07 21:26:39 +0100

    Update feasible_set_ESSEC.R

--- feasible_set_ESSEC.R
+++ feasible_set_ESSEC.R
@@ -7,22 +7,19 @@ pacman::p_load("readxl","ggplot2","tibble","dplyr","data.table")
 #############################################  FEASIBLE SET #######################################################
 
 #load data and calculate annualized historical returns and covariances
-ret <- as.matrix(read_excel("stock_prices.xlsx") %>%  select_if(is.numeric) %>% 
-                   mutate_all(~ ( (.) - shift(.))/(.)) %>% na.omit())
-moy <- 252*matrix(colMeans(ret))                                     #annualized expected returns
-cov_mat <- 252*as.matrix(cov(ret))                                   #annualized covariances
+ret <- read_excel("stock_prices.xlsx") %>%  select_if(is.numeric) %>% mutate_all(~ ( (.) - shift(.))/(.)) %>% na.omit()
+moy <- 252*colMeans(ret)                                             # Annualized expected returns
+cov_mat <- 252*cov(ret)                                              # Annualized covariances
 
 w <- list(seq(from = 0, to = 1, by = 0.1))                           # Create sets of weights for the 6 assets
 w <- expand.grid(rep(w, length(moy)))
-w <- as.matrix(w[rowSums(w)==1,])
+w <- as.matrix(w[rowSums(w)==1,])                                    # Weights sum to 1
 
-returns <- rowSums(w%*%moy)                                          # Portfolios' return
-stdev <- diag(sqrt(w%*%cov_mat%*%t(w)))                              # Portfolios' risk
-portfolio <- tibble(Return = returns, Risk = stdev)                  # Storing values in table
+means <- rowSums(w%*%moy)                                            # expected returns for each portfolio
+stdev <- diag(sqrt(w%*%cov_mat%*%t(w)))                              # stddev for each portfolio
+portfolio <- tibble(Mean = means, Risk = stdev)                      # portfolio's coordinates
 
 #Graph in the mean standard deviation space of all created portfolios
-ggplot(portfolio, aes(x = Risk, y = Return)) +
-  geom_point() +
-  scale_y_continuous(labels = scales::percent) +
-  scale_x_continuous(labels = scales::percent) +
-  labs(x = 'Annualized Standard Deviations', y = 'Annualized Returns')
+ggplot(portfolio, aes(x = Risk, y = Mean)) + geom_point() +
+  scale_y_continuous(labels = scales::percent) +  scale_x_continuous(labels = scales::percent) +
+  labs(x = 'Standard Deviations', y = 'Expected Returns')
