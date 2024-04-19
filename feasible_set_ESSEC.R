0e535116c7e83123bd377932f754141c1f74b9fc
William Arrata <125134722+WilliamArrata@users.noreply.github.com>
2024-04-19 21:32:39 +0200

    Update feasible_set_ESSEC.R

--- feasible_set_ESSEC.R
+++ feasible_set_ESSEC.R
@@ -2,7 +2,7 @@
 #####################   WILLIAM ARRATA - ESSEC PORTFOLIO MANAGEMENT COURSE WINTER 2023   #####################
 
 require("pacman")
-pacman::p_load("readxl","ggplot2","tibble","dplyr","data.table")
+pacman::p_load("readxl", "ggplot2", "tibble", "dplyr", "data.table", "stringr")
 
 #############################################  FEASIBLE SET #######################################################
 
@@ -17,9 +17,16 @@ w <- as.matrix(w[rowSums(w)==1,])                                    # Weights s
 
 means <- rowSums(w%*%moy)                                            # expected returns for each portfolio
 stdev <- diag(sqrt(w%*%cov_mat%*%t(w)))                              # stddev for each portfolio
-portfolio <- tibble(Mean = means, Risk = stdev)                      # portfolio's coordinates
+portfolio <- data.frame(Mean = means, Risk = stdev)                  # portfolio's coordinates
 
 #Graph in the mean standard deviation space of all created portfolios
-ggplot(portfolio, aes(x = Risk, y = Mean)) + geom_point() +
+coord <- data.frame(mean = moy, stddev = sqrt(diag(cov_mat))) %>% arrange(mean)
+
+ggplot(portfolio, aes(x = Risk, y = Mean)) + geom_point(color="indianred", size = 0.5) +
+  geom_point(data = coord, aes(stddev, mean)) +
+  annotate("text", x = coord$stddev, y = coord$mean, hjust = c(1.1, 0.4, rep(-0.1, 3), 1), size = 4, 
+           vjust = c(0.5, 0.2)[c(1,2,2,2,1,1)], 
+           label = as.expression(sapply(split(coord, coord$mean), function(x)
+             bquote(mu == .(round(x[[1]]*100, 1)) ~ "% ; "~ sigma == .(round(x[[2]]*100, 1)) ~ "%")))) +
   scale_y_continuous(labels = scales::percent) +  scale_x_continuous(labels = scales::percent) +
   labs(x = 'Standard Deviations', y = 'Expected Returns')
