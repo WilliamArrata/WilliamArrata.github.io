c8af6c003c9f13962f9077b1e9f7019d7bfaa987
William Arrata <125134722+WilliamArrata@users.noreply.github.com>
2024-04-21 17:42:33 +0200

    Update risk_diversification_ESSEC.R

--- risk_diversification_ESSEC.R
+++ risk_diversification_ESSEC.R
@@ -1,6 +1,3 @@
-
-#####################   WILLIAM ARRATA - ESSEC PORTFOLIO MANAGEMENT COURSE WINTER 2023   #####################
-
 require("pacman")
 pacman::p_load("data.table","dplyr","ggplot2")
 
@@ -8,7 +5,7 @@ pacman::p_load("data.table","dplyr","ggplot2")
 
 m <- c(3,7)*1e-2                                                         #Expected returns on assets A and B
 V <- c(1.44, 6.25)*1e-2                                                  #Variance on assets A and B
-rho <- c(-1, 1, 0, 0.25)                                                 #different values for the correlation
+rho <- c(-1, 1, 0, 0.25)                                                 #Various values for correlation btw A and B
 cov <- apply(cbind(V[1],replicate(2,sqrt(prod(V))*rho),V[2]),1,list)     #all covariances matrix
 
 w <- seq(0, 1, length.out = 300)
@@ -43,9 +40,9 @@ sig <- list(c(0.05,0.08,0.03), c(0.08,0.11,-0.03), c(0.11,0.12,0.06), #covarianc
           c(0.12,0.16,0.07), c(0.16,0.21,0.12), c(0.05,0.21,-0.08))
 
 #assets coordinates
-assets <- 100*data.frame(stdev = unique(sqrt(unlist(lapply(sig, function(x) head(x,2))))), 
+assets <- 100*data.frame(stdev = unique(unlist(lapply(sig, function(x) sqrt(head(x,2))))), 
                          mean = unique(unlist(mu)),
-                         correl = unlist(lapply(sig, last))/sqrt(mapply(prod, lapply(sig, function(x) head(x,2)))))
+                         correl = unlist(lapply(sig, function(x) last(x)/sqrt(prod(head(x,2))))))
 
 w_a <- seq(0, 1, length.out = 300)
 w_a <- cbind(w_a, rev(w_a))       #a range of weights for each asset in the portfolio, varying by steps of 1/300
@@ -57,32 +54,25 @@ for (i in seq_along(mu)){
   pair_tot[[i]] <- data.frame( stdev = sd_tot[[i]], mean = mu_tot[[i]])}
 
 #the resulting curves
-curve <- do.call(rbind, pair_tot[-length(pair_tot)])
-curve <- curve[order(curve$mean),]
-curve_2 <- pair_tot[length(pair_tot)][[1]]
+curve <- do.call(rbind, head(pair_tot, -1)) %>% arrange(mean)
+curve_2 <- tail(pair_tot, 1)[[1]]
 
 #Graph of curves formed by consecutive pairs of assets
 ggplot(curve, aes(stdev, mean)) +
-  xlim(c(0.9,1.1)*range(curve$stdev)) + ylim(c(0.9,1.1)*range(curve$mean)) +
   geom_point(color="blue", size = 0.5) + geom_point(data = assets, aes(stdev, mean), size=3) +
   geom_text(data = assets, aes(x=stdev, y=mean, label = LETTERS[seq_along(mu)]), hjust = -0.6, vjust = 1.2, fontface = 2) +
+  xlim(c(0.9,1.1)*range(curve$stdev)) + ylim(c(0.9,1.1)*range(curve$mean)) +
   labs(x = "standard deviation (%)", y = "expected return (%)") + theme(plot.margin = margin(1.2,.5,1.2,.5, "cm"))
 
 #Adding in the graph the curve between most distant assets
-ggplot(data =  assets, aes(x = stdev, y = mean)) +
-  xlim(c(0.9,1.1)*range(curve_2$stdev)) + ylim(c(0.9,1.1)*range(curve_2$mean)) +
-  geom_point(aes(fill = as.factor(correl))) +
-  geom_segment(data = curve, aes(x = stdev, xend = dplyr::lead(stdev), y = mean, yend = dplyr::lead(mean)), 
-               size = 1, color="indianred") +
-  geom_segment(data = curve_2, aes(x = stdev, xend = dplyr::lead(stdev), y = mean, yend = dplyr::lead(mean)),size = 1) +
-  geom_point(data = assets, aes(stdev, mean), size = 3) +
+ggplot() + geom_point(data = assets, aes(x = stdev, y = mean, fill = as.factor(correl)), size = 3) +
   annotate(geom = "text", x = assets$stdev,  y = assets$mean, label = LETTERS[seq_along(mu)], 
            fontface = 2, hjust = -0.6, vjust = 1.2) +
+  geom_segment(data = curve, aes(x = stdev, xend = dplyr::lead(stdev), y = mean, yend = dplyr::lead(mean)),
+               size = 1, color="indianred") +
+  geom_segment(data = curve_2, aes(x = stdev, xend = dplyr::lead(stdev), y = mean, yend = dplyr::lead(mean)),size = 1) +
   scale_fill_manual(
     values = rep("black", 6), labels = parse(text = sprintf("rho == %s", round(assets$correl/100,2), drop = F))) +
-  # scale_fill_manual(
-  #   values = rep("black",6),
-  #   labels = paste0(expression(rho),"_",LETTERS[seq_along(mu)],",",shift(LETTERS[seq_along(mu)], places = -1)," : ",
-  #                   round(correl,2)), drop = F) +
-    labs(x = "standard deviation (%)", y = "expected return (%)", fill=NULL) +
+  labs(x = "standard deviation (%)", y = "expected return (%)", fill=NULL) +
+  xlim(c(0.9,1.1)*range(curve_2$stdev)) + ylim(c(0.9,1.1)*range(curve_2$mean)) +
   theme(legend.position = "bottom", plot.margin = margin(1.2,.5,1.2,.5, "cm"))
