f43488cebac1e09811f1f8fd1e0d4e165daf2bc8
William Arrata <125134722+WilliamArrata@users.noreply.github.com>
2024-09-15 14:37:54 +0200

    Update Risk_Neutral_Density_bond_futures.R

--- Risk_Neutral_Density_bond_futures.R
+++ Risk_Neutral_Density_bond_futures.R
@@ -91,7 +91,7 @@ for (m in 1:length(charac$terms)){
   P <- prices$put_price                                            #prices of puts for maturity m
   KC <- KP <- prices$call_strike                                   #strikes of options for maturity m
   FWD <- charac$fut_price[m]/100                                   #future price for maturity m
-  range_px[[m]] <- x_axis*range(KC, na.rm = T)               #the range of strike for matu m
+  range_px[[m]] <- x_axis*range(KC, na.rm = T)                    #the augmented range of strike for mat
   PX[[m]] <- Reduce(seq, 1e4*range_px[[m]])*1e-4                   #values of x to comput PDF and CDF
   nb_opt[[m]] <- nrow(prices)                                      #number of options for matu m
   
@@ -236,7 +236,7 @@ mapply(function(x,y) sum(rollmean(x, 2)*diff(y)), DNR, PX)   #check that integra
 
 #Graph of risk neutral densities for a sum of 2 or 3 lognormals
 co <- rainbow(nrow(charac))
-xlim <- range(PX)
+xlim <- range(PX, na.rm = T)
 ylim <- range(DNR)
 series <- mapply(cbind, PX, DNR)
 
@@ -249,7 +249,8 @@ plot(NA, pch = 20, xlab = "", ylab = "density", main = paste("RNDs from a mixtur
      xlim = xlim, ylim = ylim, las = 1)
 mapply(lines, series, col = co)
 title(sub = "OAT future price (% of par)", adj = 1, line = 2)
-legend("bottom", inset = c(-0.05, -0.3), legend = charac$option_matu, horiz = T, col = co, lty = 1, bty = "n")
+legend("bottom", inset = c(-0.05, -0.4), legend = format(as.yearmon(charac$option_matu), "%b %y"),
+       horiz = T, col = co, lty = 1, bty = "n")
 
 #######################  CALCULATION OF ACCRUED COUPON OF CTDs AT OPTION MATURITY ###########################
 
@@ -257,14 +258,14 @@ legend("bottom", inset = c(-0.05, -0.3), legend = charac$option_matu, horiz = T,
 
 bond_fut <- read_excel("inputs/OATA_fut_characteristics.xlsx", 1) %>%  
   rename_with(~c("fut_contract", "conv_factor", "ctd_cp", "ctd_matu")) %>% 
-  mutate(fut_contract = word(fut_contract,1)) %>%  filter(fut_contract%in%charac$fut_contract) %>% 
+  mutate_at("fut_contract", ~word(., 1)) %>%  filter(fut_contract%in%charac$fut_contract) %>% 
   mutate_at("ctd_matu", as.Date, format = "%d/%m/%Y") %>% left_join(charac) %>% 
   mutate(prev_cp_dt = as.Date(paste0(format(option_matu, "%Y"),"-",format(ctd_matu, "%m-%d")))) %>% 
   mutate(prev_cp_dt = ifelse(as.numeric(option_matu - prev_cp_dt) < 0, 
                              as.Date(paste0(as.numeric(format(option_matu, "%Y")) - 1, "-", format(ctd_matu, "%m-%d"))),
                              prev_cp_dt)) %>% mutate_at("prev_cp_dt", as.Date) %>% 
-  mutate(cc = as.numeric((option_matu - prev_cp_dt )/365)) %>% mutate(acc = ctd_cp*cc) %>%
-  mutate(years = as.numeric(ctd_matu - option_matu)/365 ) %>% mutate(PX_liv = fut_price*conv_factor + acc)
+  mutate(cc = as.numeric((option_matu - prev_cp_dt )/365), acc = ctd_cp*cc, 
+         PX_liv = fut_price*conv_factor + acc, years = as.numeric(ctd_matu - option_matu)/365 )
 
 ####################  CONVERSION OF FUTURES PRICES INTO CTD PRICES THEN YIELDS AT MATU #######################
 
@@ -295,30 +296,43 @@ for (i in 1:nrow(bond_fut)){
 
 #############################  STATISTICS OF THE DISTRIBUTION #############################
 
-#mean of the yield distribution at each options' maturity
-E_y <- mapply(function(x, y, z) sum(rollmean(x*y, 2)*diff(z)), tri, DNR, PX)
+#mean of futures prices at each options's maturity
+E <- mapply(function(x, y, z) sum(rollmean(x*y, 2)*diff(z)), PX, DNR, PX)
 
-#taux moyens implicites aux futures. NB: seule la moyenne est récupérable ici, pas la distribution
-y_fut <- list()
-for (i in 1:nrow(bond_fut)){
-  y_fut[[i]] <- xirr(cf = c(-bond_fut$PX_liv[i], cf[[i]], N[i]), tau = c(0, term[[i]][[1]]), comp_freq = 1, 
-                     interval = c(0, 10))}
+#mean of CtD ytm from mean of futures prices at each option's maturity
+E_y <- mapply(function(x, y, z, t, u, v) 
+  xirr(cf = c(-(100*x*y+z), t, u), tau = c(0, v[[1]]), comp_freq = 1, interval = c(0, 10)),
+  E, bond_fut$conv_factor, bond_fut$acc, cf, N, term)
 
-ecart <- unlist(y_fut) - E_y     #ecart entre les distributions fittées et le taux future implicite
+#consistency check : ytm implicit to future contracts prices
+y_fut <- mapply(function(x, y, z, t, u, v) 
+  xirr(cf = c(-(x*y+z), t, u), tau = c(0, v[[1]]), comp_freq = 1, interval = c(0, 10)),
+  bond_fut$fut_price, bond_fut$conv_factor, bond_fut$acc, cf, N, term)
 
-tri <- mapply("+", tri, ecart)       #on décale toute la distribution de cet écart
-
-#graph de densité des taux implicites
-co <- rainbow(nrow(charac))
-xlim <- range(tri, na.rm = T)
+#graph of implied ytm densities in base R
+co <- rainbow(nrow(charac_1))
+xlim_r <- range(tri, na.rm = T)
 series_rev <- mapply(cbind, tri, DNR)
 
 par(mar=c(7,4,4,4) + 0.1, xpd = T, cex.axis = cex)
-plot(NA, pch = 20, xlab = "OAT future yield (%)", ylab = "density", xlim = xlim, ylim = ylim, las = 1, 
+plot(NA, pch = 20, xlab = "OAT future yield (%)", ylab = "density", xlim = xlim_r, ylim = ylim, las = 1, 
      main = "RNDs from a mixture of 2 lognormals")
 mapply(lines, series_rev, col = co)
 legend("bottom", inset = c(-0.05,-0.2), legend = charac$option_matu, horiz = T, col=co, lty = 1, bty = "n")
 
+#graph of implied ytm densities using ggplot
+series_rev <- lapply(series_rev, data.frame)
+
+ggplot() + geom_line(data = series_rev[[1]], aes(x= X1, y = X2,  colour =  as.character(charac$option_matu[1]))) +
+  geom_line(data = series_rev[[2]], aes(x= X1, y = X2,  colour = as.character(charac$option_matu[2]))) +
+  geom_line(data = series_rev[[3]], aes(x= X1, y = X2,  colour = as.character(charac$option_matu[3]))) +
+  geom_line(data = series_rev[[4]], aes(x= X1, y = X2,  colour = as.character(charac$option_matu[4]))) +
+  labs(title = "OAT future ytm (%)", subtitle = "Probability density functions") +
+  labs(y = "probability density", x = "Cheapest-to-Deliver ytm") + 
+  scale_x_continuous(labels = scales::percent, breaks = scales::pretty_breaks(n = 7)) +
+  theme(legend.position = "bottom", legend.title=element_blank(), plot.margin = margin(.8,.5,.8,.5, "cm"))
+
+#Cumulative Density Function
 sub_2 <- function(x, y){ x[3]*plnorm(y, meanlog = x[1], sdlog = x[2]) }
 
 CDF <- function(x, y){
@@ -328,61 +342,99 @@ CDF <- function(x, y){
 
 #Graph of cumulative density functions for rates
 NCDF <- mapply(CDF, params, PX)
-NCDF_rev <- sapply(NCDF, rev)
-tri_rev <- sapply(tri, rev)
-series_CDF <- mapply(cbind, tri_rev, NCDF)
+series_CDF <- mapply(cbind, PX, NCDF)
+series_CDF_tri <- mapply(cbind, tri, NCDF)
 
-par(mar=c(7,6,4,4) + 0.1, xpd=T, cex.axis=cex)
-plot(NA, pch = 20, xlab = "", ylab = "cumulative probability", xlim = xlim, ylim = c(0, 1), las = 1,
-     main=paste("RNDs from a mixture of",nb_log,"lognormals"))
+par(mar = c(7, 6, 4, 4) + 0.1, xpd = T, cex.axis = cex)
+plot(NA, pch = 20, xlab = "", ylab = "cumulative probability", xlim = xlim, ylim = 0:1, las = 1,
+     main = paste("RNDs from a mixture of", nb_log, "lognormals"))
 mapply(lines, series_CDF, col = co)
-title(sub="UST future yield (%)",adj =1,line=2)
-legend("bottom", inset = c(-0.05,-0.35), legend = charac$option_matu, horiz = T,col=co, lty = 1, bty = "n")
+title(sub = "OAT future price (% of par)", adj = 1, line = 2)
+legend("bottom", inset = c(-0.05,-0.35), legend = charac$option_matu, horiz = T,col = co, lty = 1, bty = "n")
+
+par(mar = c(7, 6, 4, 4) + 0.1, xpd = T, cex.axis = cex)
+plot(NA, pch = 20, xlab = "", ylab = "cumulative probability", xlim = xlim_r, ylim = 0:1, las = 1,
+     main = paste("RNDs from a mixture of", nb_log, "lognormals"))
+mapply(lines, series_CDF_tri, col = co)
+title(sub = "OAT future price (% of par)", adj = 1, line = 2)
+legend("bottom", inset = c(-0.05,-0.35), legend = charac$option_matu, horiz = T,col = co, lty = 1, bty = "n")
+
 
 #Standard deviation, skewness and kurtosis for the distribution at each options' maturity
 moments <- function(x){
-  return(mapply(function(x, y, z, t, u) sum(rollmean(((t-y)^x)*z, 2)*diff(u)), x, E_y, DNR, tri, PX))}
+  return(mapply(function(x, y, z, t) sum(rollmean( z*(t - y)^x , 2)*diff(t)), x, E, DNR, PX))}
 
-SD_y <- sqrt(moments(2))
-SK_y <- moments(3)/SD_y^3
-KU_y <- moments(4)/SD_y^4
+SD <- sqrt(moments(2))
+SK <- moments(3)/SD^3
+KU <- moments(4)/SD^4
 
 #all statistics at a glance
 desc_stats <- bond_fut %>% bind_cols(t(sapply(range_px, function(x) x/x_axis))) %>%
-  rename_at(14:15, ~c("min_strike", "max_strike")) %>%
-  mutate_at(vars(contains("strike")), ~ .*conv_factor + acc) %>%
-  mutate(fut_rate = 100*unlist(y_fut)) %>%
-  mutate_at(vars(contains("strike")), ~ 100*xirr(cf = c(-., rep(ctd_cp, trunc(years)), 100 + ctd_cp),
-                                                 tau = c(0, (0:years) + years - trunc(years)), comp_freq = 1, interval = c(0, 10))) %>%
-  select(c(fut_contract, option_matu, fut_rate, min_strike, max_strike)) %>%
-  bind_cols(100*E_y, 100*SD_y, SK_y, KU_y, nb_opt = unlist(nb_opt)) %>%
-  rename_at(6:9, ~c("mean (%)", "stddev (%)", "skewness", "kurtosis"))
+  rename_at(14:15, ~c("min_strike", "max_strike")) %>% 
+  mutate_at(vars(contains("strike")), ~ .*conv_factor + acc) %>% 
+  mutate_at(vars(contains("strike")), ~ 100*xirr(cf = c(-., rep(ctd_cp, trunc(years)), 100 + ctd_cp), 
+                                                 tau = c(0, (0:years) + years - trunc(years)),
+                                                 comp_freq = 1, interval = c(0, 10))) %>% 
+  select(c(fut_contract, option_matu, fut_rate, min_strike, max_strike)) %>% 
+  bind_cols(100*E_y, SD, SK, KU, nb_opt = unlist(nb_opt)) %>%
+  rename_at(6:9, ~c("mean (%)", "stddev (%)", "skewness", "kurtosis")) 
 
 #a few quantiles
-nb_q <- 1000
-thres <- rev(seq(1:nb_q)/nb_q)
+nb_q <- 10
+thres <- seq(nb_q)/nb_q
 quantiles <- list()
 for (i in 1:nrow(charac)){
-  quantiles[[i]]<-list()
+  quantiles[[i]] <- list()
   for (j in 1:length(thres)){
-    quantiles[[i]][[j]] <- 100*mean(tri[[i]][c(min(which(NCDF[[i]]>thres[j] - 0.01)),
-                                             max(which(NCDF[[i]]<thres[j] + 0.01)))]) 
+    quantiles[[i]][[j]] <- mean(tri[[i]][c(min(which(NCDF[[i]] > thres[j] - 1e-5)),
+                                           max(which(NCDF[[i]] < thres[j] + 1e-5)))])
   }
-  quantiles[[i]]<-unlist(quantiles[[i]])
+  quantiles[[i]] <- unlist(quantiles[[i]])
 }
 
-#graph of a few quantiles for all RNDS (through time)
-bind_cols(rep(charac$terms, lengths(quantiles)), 1-unlist(quantiles), rep(rev(paste0("q",nb_q*thres)),
-  length(quantiles))) %>% rename_all(~c("term", "value", "quantile")) %>% 
-  ggplot(aes(term, value, fill = quantile)) + geom_line() +
-  labs(x = "term (years)", y = "10Y OAT rate (%)") + theme(plot.margin = margin(1.2,.5,1.2,.5, "cm"))
+rate_spot <- 0.02848
 
-#graph a given quantile for a given RND
-cutoff <- quantile(tri[[3]], probs = 0.65)
+#graph of quantiles through time with shaded areas
+quantiles_2 <- bind_cols(c(0, charac$terms), rbind(rate_spot, do.call(rbind, quantiles))) %>%
+  rename_with(~c("term", paste0("q", nb_q*thres)))
 
-data.frame(x = tri[[3]], y = rev(DNR[[3]])) %>% mutate(area = x > cutoff) %>% 
-  ggplot(aes(x = x, ymin = 0, ymax = y, fill = area)) + geom_ribbon() + geom_line(aes(y = y)) +
-  annotate(geom = 'text', x = cutoff, y = 0.025, label = 'quantile of order 65%', hjust = -0.1) +
+ggplot(quantiles_2, aes(x = term)) +
+  geom_ribbon(aes(ymin = q1, ymax = q9, fill = "80%-90%"), alpha=0.2) +
+  geom_ribbon(aes(ymin = q1, ymax = q8, fill = "70%-80%"), alpha=0.2) +
+  geom_ribbon(aes(ymin = q1, ymax = q7, fill = "60%-70%"), alpha=0.2) +
+  geom_ribbon(aes(ymin = q1, ymax = q6, fill = "50%-60%"), alpha=0.2) +
+  geom_ribbon(aes(ymin = q1, ymax = q5, fill = "40%-50%"), alpha=0.2) +
+  geom_ribbon(aes(ymin = q1, ymax = q4, fill = "30%-40%"), alpha=0.2) +
+  geom_ribbon(aes(ymin = q1, ymax = q3, fill = "20%-30%"), alpha=0.2) +
+  geom_ribbon(aes(ymin = q1, ymax = q2, fill = "10%-20%"), alpha=0.2) +
+  labs(x = "term (years)", y = "10Y OAT rate (%)") + scale_y_continuous(labels = scales::percent) +
+  theme(legend.position= "bottom", legend.title=element_blank(), plot.margin = margin(1.2,.5,1.2,.5, "cm"))
+
+#graph of quantiles through time unshaded
+quantiles_3 <- bind_cols(rep(c(0, charac$terms), c(unique(lengths(quantiles)), lengths(quantiles)) ),
+                         c(rep(rate_spot, unique(lengths(quantiles))), unlist(quantiles)),
+                         rep(rev(paste0("q", nb_q*thres)), 1+ length(quantiles))) %>% 
+  rename_all(~c("term", "value", "quantile"))
+
+ggplot(quantiles_3, aes(term, value, color = quantile)) +  geom_line() +
+  labs(x = "term (years)", y = "10Y OAT rate (%)") + 
+  theme(legend.position= "bottom", plot.margin = margin(1.2,.5,1.2,.5, "cm")) +
+  scale_y_continuous(labels = scales::percent) 
+
+
+#graph of quantile of order q for a given maturity
+q <- 90
+d <- 2
+cutoff <- mean(PX[[d]][c(min(which(NCDF[[d]] > q/100 - 1e-5)),
+                         max(which(NCDF[[d]] < q/100 + 1e-5)))])
+dnr_q <- data.frame(x = PX[[d]], y = DNR[[d]]) %>% mutate(area = x > cutoff)
+
+ggplot(data = dnr_q, aes(x = x)) + geom_ribbon(aes(ymin = 0, ymax = y, fill = area)) +
+  geom_line(aes(y = y)) +
+  annotate(geom = 'text', x = cutoff, y = -1, label = paste0("q",q), hjust = 0.5) +
   scale_x_continuous(labels = scales::percent, breaks = scales::pretty_breaks(n =6)) +
-  labs(x = paste0('OAT 10Y future ', charac$option_matu[3]), y = 'probability density') +
+  labs(x = '10-Y OAT contract future price (%)', y = 'probability density', 
+       title = paste0("10-Y OAT contract RND and quantile of order ", q, "%"),
+       subtitle = paste0("Probability density ", charac$option_matu[d])) +
+  theme_light() +
   theme(legend.position = "none", plot.margin = margin(.8,.5,.8,.5, "cm"))
